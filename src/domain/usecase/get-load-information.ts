09d80f3f74108c233ab8ab5b61bba70e64f49cec
jellan bozo <jellan.bozo0@walmart.com>
2022-03-07 18:48:41 -0300

    refactor enviroment variable to execute payment

--- src/domain/usecase/get-load-information.ts
+++ src/domain/usecase/get-load-information.ts
@@ -69,7 +69,6 @@ export async function getLoadInformation(
             guildName: 'Not Occupied'
         })
     }
-
     return {
         usersOnline: usersQuantityOnline,
         usernameLastUniqueKill: username[0],
